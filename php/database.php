8758d310e80b2e00fb19906cf9ea81453a456722
Diego Herrera <goherrerago@gmail.com>
2018-11-29 22:12:54 -0600

    Agregando conexión a base de datos

--- php/database.php
+++ php/database.php
@@ -1,14 +1,21 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'solicitudes';
-$user = 'root';
-$pass = '';
-$charset = 'utf8';
+    $host = '127.0.0.1';
+    $db   = 'solicitudes';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new \PDO($dsn, $user, $pass, $opt);
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+
+    try {
+        $dbConnect = new \PDO($dsn, $user, $pass, $options);
+    } catch (\Exception $e) {
+        print "!Error¡: " . $e->getMessage() . "</br>";
+        die();
+    }
+?>
